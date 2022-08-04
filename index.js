baec9895bb668441c12171addfaee3b2defa672b
jamshidev404 <jamshidbek536@gmail.com>
2022-08-04 11:38:14 +0500

    background

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const theme = document.querySelector("#theme");
 const themeModal = document.querySelector(".customize-theme");
 const fontSizes = document.querySelectorAll(".choose-size span");
 var root = document.querySelector(":root");
-const colorPalette = document.querySelector('.choose-color span') 
+const colorPalette = document.querySelector(".choose-color span");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
 
 // ============ SIDEBAR ===========
 // remove active class from all menu items
@@ -124,4 +127,90 @@ fontSizes.forEach((size) => {
     //change font size of the root html element
     document.querySelector("html").style.fontSize = fontSize;
   });
+});
+
+//remove active class from colors
+const changeActiveClassColors = () => {
+  colorPalette.forEach((colorPicker) => {
+    colorPicker.classList.remove("active");
+  });
+};
+
+/// change primary colors
+colorPalette.forEach((color) => {
+  color.addEventListener("click", () => {
+    let primary;
+    //remove active class from colors
+    changeActiveClassColors();
+
+    if (color.classList.contains("color-1")) {
+      primaryHue = 252;
+    } else if (color.classList.contains("color-2")) {
+      primaryHue = 52;
+    } else if (color.classList.contains("color-3")) {
+      primaryHue = 352;
+    } else if (color.classList.contains("color-4")) {
+      primaryHue = 152;
+    } else if (color.classList.contains("color-5")) {
+      primaryHue = 202;
+    }
+    color.classList.add("active");
+
+    root.style.setProperty("--primary-color-hue", primaryHue);
+  });
+});
+
+//theme BACKGROUND values
+let lightColorLightless;
+let darkColorLightless;
+let whiteColorLightless;
+
+// change background color 
+const changeBG = () => {
+  root.style.setProperty('--light-color-lightness', lightColorLightless)
+  root.style.setProperty('--white-color-lightness', whiteColorLightless)
+  root.style.setProperty('--dark-color-lightness', darkColorLightless)
+}
+
+Bg1.addEventListener("click", () => {
+
+  //add active class
+  Bg1.classList.add("active");
+
+  //remove active class from others
+  Bg2.classList.remove("active");
+  Bg3.classList.remove("active");
+
+  // remove customized changes from local storage
+  window.location.reload()
+});
+
+Bg2.addEventListener("click", () => {
+  lightColorLightless = "15%";
+  darkColorLightless = "95%";
+  whiteColorLightless = "20%";
+
+  //add active class
+  Bg2.classList.add("active");
+
+  //remove active class from others
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+
+  changeBG();
+});
+
+Bg3.addEventListener("click", () => {
+  lightColorLightless = "0%";
+  darkColorLightless = "95%";
+  whiteColorLightless = "20%";
+
+  //add active class
+  Bg2.classList.add("active");
+
+  //remove active class from others
+  Bg1.classList.remove("active");
+  Bg3.classList.remove("active");
+
+  changeBG();
 });
